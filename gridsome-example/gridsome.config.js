4c4b7ce66dc9b62680f8126b0d7098557515b7d7
avnikasikciwin <avni.kasikci.01@gmail.com>
2022-10-23 17:34:37 +0300

    delete file and create markdown post

--- gridsome-example/gridsome.config.js
+++ gridsome-example/gridsome.config.js
@@ -6,5 +6,40 @@
 
 module.exports = {
   siteName: 'Avni Kasikci',
-  plugins: []
+  siteDescription: 'Full Stack Developer',
+
+  templates: {
+    Post: '/:title',
+    Tag: '/tag/:id'
+  },
+
+  plugins: [
+    {
+      // Create posts from markdown files
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Post',
+        path: '/static/content/posts/*.md',
+        refs: {
+          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    }
+  ],
+
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
 }
